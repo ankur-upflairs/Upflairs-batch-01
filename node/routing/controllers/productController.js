87d45222d99e0b088eaec1692b94c17ed6dc115c
ankur-upflairs <ankur.upflairs@gmail.com>
2024-06-26 12:01:03 +0530

    fjdk

--- node/routing/controllers/productController.js
+++ node/routing/controllers/productController.js
@@ -1,15 +1,13 @@
-let express= require('express')
-let router=express.Router()
 const {products}=require('../products.js')
 
-router.get('/',(req,res)=>{
+exports.allProducts=(req,res)=>{
     res.status(200).json({
         status:'success',
         data:products
     })
-})
+}
 
-router.get('/:id',(req,res)=>{
+exports.singleProduct=(req,res)=>{
     let {id}=req.params
     let product=products.find((v,i)=>v.id==id)
     if(product) res.status(200).json({
@@ -20,21 +18,38 @@ router.get('/:id',(req,res)=>{
         status:'failed',
         msg:'product not found'
     })
-})
+}
 
-router.post('/',(req,res)=>{
-    // console.log(req.body)
+exports.addProduct=(req,res)=>{
+    console.log(req.body)
     let {name}=req.body
+
     let id= products[products.length-1].id + 1
     products.push({id,name})
     res.json({
         status:'success',
         data:{id,name}
     })
-})
+}
 
-// dynamic routing
-router.delete('/:id',(req,res)=>{
+exports.updateProduct=(req,res)=>{
+    let {id}=req.params 
+    let {name}=req.body
+    let product=products.find((v,i)=>v.id==id)
+    if(product) {
+        let index= products.indexOf(product)
+         products.splice(index,1,{id,name})        
+        res.status(200).json({
+        status:'success',
+        msg:'product updated'
+    })}
+    else res.status(400).json({
+        status:'failed',
+        msg:'product not found'
+    })
+}
+
+exports.deleteProduct=(req,res)=>{
     let {id}=req.params 
 
     let product=products.find((v,i)=>v.id==id)
@@ -50,22 +65,6 @@ router.delete('/:id',(req,res)=>{
         msg:'product not found'
     })
     
-})
-router.put('/:id',(req,res)=>{
-    let {id}=req.params 
-    let {name}=req.body
-    let product=products.find((v,i)=>v.id==id)
-    if(product) {
-        let index= products.indexOf(product)
-         products.splice(index,1,{id,name})        
-        res.status(200).json({
-        status:'success',
-        msg:'product updated'
-    })}
-    else res.status(400).json({
-        status:'failed',
-        msg:'product not found'
-    })
-})
+}
 
-module.exports=router
+
--- node/routing/server.js
+++ node/routing/server.js
@@ -1,8 +1,16 @@
 const express=require('express')
+const cors=require('cors')
+
 const app=express()
 
-const productRoutes=require('./router/product.js')
+const productRoutes=require('./routes/product.js')
 
+app.use(cors())
+// app.use(cors({
+//     origin:'*',
+//     methods:['GET'],
+//     allowedHeaders:[]
+// }))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
